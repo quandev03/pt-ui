d4a73f339d04ef4049adf66d633427a098f5ba58
PhucMN <phucmn@vissoft.vn>
2025-08-21 16:33:33 +0700

    update config prod

--- apps/Partner/src/constants/index.ts
+++ apps/Partner/src/constants/index.ts
@@ -17,7 +17,7 @@ export const APP_VERSION = (
   window as unknown as { _env_: { [key: string]: string } }
 )._env_?.VITE_APP_VERSION;
 
-export const APP_CODE = 'hivn-partner';
+export const APP_CODE = 'vnsky-agent';
 
 export const STORAGE_KEY_PREFIX =
   (window as unknown as { _env_: { [key: string]: string } })._env_
