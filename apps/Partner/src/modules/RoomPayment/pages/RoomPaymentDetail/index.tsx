a96690cf42ba7d7f11dad3458d4d9266b25bab16
Ngô Hồng Quân <QuanNH@vissoft.vn>
2025-11-23 02:27:56 +0700

    fix

--- apps/Partner/src/modules/RoomPayment/pages/RoomPaymentDetail/index.tsx
+++ apps/Partner/src/modules/RoomPayment/pages/RoomPaymentDetail/index.tsx
@@ -9,7 +9,7 @@ import { useLogicRoomPaymentDetail } from './useLogicRoomPaymentDetail';
 import { ColumnsType } from 'antd/es/table';
 import { IRoomPaymentDetail } from '../../types';
 import { PaymentStatusMap, PaymentStatus } from '../../constants/enum';
-import { useGetAgencyOptions } from '../../../hooks/useGetAgencyOptions';
+import { useGetAgencyOptions } from '../../../../hooks/useGetAgencyOptions';
 
 export const RoomPaymentDetail = memo(() => {
   const {
